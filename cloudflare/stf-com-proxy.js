574ab680468c496f5ee8d4796676b4fb01a790bd
Fabio Nardoni <f.nardoni@outlook.com.br>
2026-08-07 00:44:43 -0300

    pin Worker to fbef42035f390284b63ead5d2c2d0c2b5ae8d45e (Uber 8km + frete copy)

--- cloudflare/stf-com-proxy.js
+++ cloudflare/stf-com-proxy.js
@@ -4,7 +4,7 @@
  * - .com.br → Portuguese (repo root)
  * IMPORTANT: pin COMMIT after each push so domains are not stuck on stale @main cache.
  */
-const COMMIT = '2c1ef49dbe0a0fc4efcc8ed024012119a7931e3f';
+const COMMIT = 'fbef42035f390284b63ead5d2c2d0c2b5ae8d45e';
 const ORIGINS = [
   'https://cdn.jsdelivr.net/gh/nardoniF/site-sensortattoofix@' + COMMIT,
   'https://raw.githubusercontent.com/nardoniF/site-sensortattoofix/' + COMMIT,
